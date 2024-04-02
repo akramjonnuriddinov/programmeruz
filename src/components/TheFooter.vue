cc6f1d3ac9ff369e722a677b7095460defd542b9
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-04-02 03:22:09 -0700

    Add social links to TheFooter

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { getImageUrl } from '@/composables/getImageUrl.js'
+
 const productLinks = [
   {
     name: 'Team Boards ',
@@ -87,6 +89,25 @@ const resourceLinks = [
     path: '#'
   }
 ]
+
+const socials = [
+  {
+    name: 'social/Twitter.svg',
+    path: '#'
+  },
+  {
+    name: 'social/Facebook.svg',
+    path: '#'
+  },
+  {
+    name: 'social/Linkedin.svg',
+    path: '#'
+  },
+  {
+    name: 'social/Instagram.svg',
+    path: '#'
+  }
+]
 </script>
 
 <template>
@@ -96,53 +117,77 @@ const resourceLinks = [
         class="container flex flex-col flex-wrap py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap"
       >
         <div class="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:mt-0 md:text-left">
-          <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+          <div class="w-full px-4 lg:w-1/5 md:w-1/2">
             <h2
               class="mb-3 text-sm font-medium tracking-widest text-[#787878] uppercase title-font"
             >
               Product
             </h2>
-            <nav class="mb-10 list-none">
+            <ul class="mb-10">
               <li v-for="productLink in productLinks" :key="productLink.name" class="mt-3">
                 <router-link to="/" class="cursor-pointer">{{ productLink.name }}</router-link>
               </li>
-            </nav>
+            </ul>
           </div>
-          <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+          <div class="w-full px-4 lg:w-1/5 md:w-1/2">
             <h2
               class="mb-3 text-sm font-medium tracking-widest text-[#787878] uppercase title-font"
             >
               Company
             </h2>
-            <nav class="mb-10 list-none">
+            <ul class="mb-10">
               <li v-for="companyLink in companyLinks" :key="companyLink.name" class="mt-3">
                 <router-link to="/" class="cursor-pointer">{{ companyLink.name }}</router-link>
               </li>
-            </nav>
+            </ul>
           </div>
-          <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+          <div class="w-full px-4 lg:w-1/5 md:w-1/2">
             <h2
               class="mb-3 text-sm font-medium tracking-widest text-[#787878] uppercase title-font"
             >
               Support
             </h2>
-            <nav class="mb-10 list-none">
+            <ul class="mb-10">
               <li v-for="supportLink in supportLinks" :key="supportLink.name" class="mt-3">
                 <router-link to="/" class="cursor-pointer">{{ supportLink.name }}</router-link>
               </li>
-            </nav>
+            </ul>
           </div>
-          <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+          <div class="w-full px-4 lg:w-1/5 md:w-1/2">
             <h2
               class="mb-3 text-sm font-medium tracking-widest text-[#787878] uppercase title-font"
             >
               Recources
             </h2>
-            <nav class="mb-10 list-none">
+            <ul class="mb-10">
               <li v-for="resourceLink in resourceLinks" :key="resourceLink.name" class="mt-3">
                 <router-link to="/" class="cursor-pointer">{{ resourceLink.name }}</router-link>
               </li>
-            </nav>
+            </ul>
+          </div>
+          <div class="w-full px-4 lg:w-1/5 md:w-1/2">
+            <h2
+              class="mb-8 text-sm font-medium tracking-widest text-[#787878] uppercase title-font"
+            >
+              Social Media
+            </h2>
+            <ul class="flex gap-4 mb-10">
+              <li v-for="social in socials" :key="social.name" class="mt-3">
+                <a :href="social.path">
+                  <img :src="getImageUrl(social.name)" alt="" />
+                </a>
+              </li>
+            </ul>
+            <div>
+              <a href="#" class="mb-4 flex">
+                <img class="mr-2" src="@/assets/img/message.svg" alt="" />
+                contact@macrivate.com
+              </a>
+              <a href="#" class="flex items-center">
+                <img class="mr-2" src="@/assets/img/phone.svg" alt="" />
+                +123 456 789
+              </a>
+            </div>
           </div>
         </div>
       </div>
