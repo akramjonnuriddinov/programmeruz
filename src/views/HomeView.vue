1dd52ccc7446038074998a9ef1317672b9ea8905
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-04-02 01:46:30 -0700

    Add GoalComponent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import HomeHero from '@/components/HomeHero.vue'
 import StatsComponent from '@/components/StatsComponent.vue'
 import EngagementComponent from '@/views/home/EngagementComponent.vue'
 import PerformanceComponent from '@/views/home/PerformanceComponent.vue'
+import GoalComponent from '@/views/home/GoalComponent.vue'
 </script>
 
 <template>
@@ -10,4 +11,5 @@ import PerformanceComponent from '@/views/home/PerformanceComponent.vue'
   <stats-component />
   <engagement-component />
   <performance-component />
+  <goal-component />
 </template>
---
+++ src/views/home/GoalComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="bg-tg-white py-10">
+    <div class="container mx-auto w-full px-5">
+      <div class="flex items-center">
+        <div class="relative z-10">
+          <img class="w-[555px] h-[660px]" src="@/assets/img/working-girl-3.jpg" alt="" />
+          <div
+            class="flex rounded-full bg-white w-full max-w-[330px] p-6 absolute bottom-16 left-1/2 -translate-x-1/2 card"
+          >
+            <img class="mr-4" src="@/assets/img/pie39.png" alt="" />
+            <div>
+              <h5 class="text-sm font-medium mb-2">
+                <span class="font-semibold">Significantly</span> exceeded business goals
+              </h5>
+              <p class="text-xs">Past 12 month</p>
+            </div>
+          </div>
+        </div>
+        <div class="relative ml-12 w-full max-w-[600px] px-4">
+          <span class="mb-4 text-base font-semibold leading-[20px] text-primary"
+            >Goals & OKR’s</span
+          >
+          <h2 class="mb-6 text-[56px] text-black font-semibold leading-[69px]">
+            Organize your team based on what matters most to you.
+          </h2>
+          <p class="mb-8 text-lg">
+            Ensure alignment across your whole organization by making strategic objectives crystal
+            clear. Do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <ul class="mb-14">
+            <li
+              v-for="aboutContent in aboutContents"
+              :key="aboutContent"
+              class="mb-6 flex items-center"
+            >
+              <div class="mr-3 w-6 h-6 flex items-center justify-center rounded-full bg-[#D4F0F0]">
+                <img class="" src="@/assets/img/check.svg" alt="" />
+              </div>
+              <p class="leading-[1.8] text-black">
+                {{ aboutContent }}
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const aboutContents = ref(['Organize your data', 'Work with any team', 'Business analytics'])
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 12px 24px 48px 0px #0000001f;
+}
+</style>
