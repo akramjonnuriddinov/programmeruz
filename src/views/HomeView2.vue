fd8aa8cb61d24b21dee9a732348c7cd9b5697767
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-04-03 09:38:59 +0500

    Fix HomeView component position

--- src/views/HomeView2.vue
+++ src/views/HomeView2.vue
@@ -2,10 +2,9 @@
 import HomeHero2 from '@/components/HomeHero2.vue'
 import TrustedIndustry from '@/components/TrustedIndustry.vue'
 import FeaturesComponent from '@/components/FeaturesComponent.vue'
-import StatsComponent from '@/components/StatsComponent.vue'
-import EngagementComponent from '@/components/EngagementComponent.vue'
-import PerformanceComponent from '@/components/PerformanceComponent.vue'
 import GoalComponent from '@/components/GoalComponent.vue'
+import StatsComponent from '@/components/StatsComponent.vue'
+import PerformanceComponent from '@/components/PerformanceComponent.vue'
 import ContactUs from '@/components/ContactUs.vue'
 import IntegrationComponent from '@/components/IntegrationComponent.vue'
 import BlogComponent from '@/components/BlogComponent.vue'
@@ -15,11 +14,9 @@ import BlogComponent from '@/components/BlogComponent.vue'
   <home-hero2 />
   <trusted-industry />
   <features-component />
-  <engagement-component />
-  <performance-component />
   <goal-component />
+  <performance-component />
   <integration-component />
-  <!-- TestimonialReview -->
   <stats-component />
   <blog-component />
   <contact-us />
